f6efda18bdb00e56873081e0529e2d6f6c21df00
Leandro Furtado <le_furtado@outlook.com.br>
2023-02-21 11:17:45 -0300

    add: footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import './styles.scss';
 
 export default function Footer() {
     return (
-        <div className='footer'>
+        <div className='footer flex'>
             <p>Orgulhosamente feito por <span>Leandro Furtado</span></p>
         </div>
     )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,17 +2,23 @@ import Header from '../../components/Header';
 import Contact from '../../components/Contact';
 import About from '../../components/About';
 import ContactIcons from '../../components/ContactIcons';
+import Footer from '../../components/Footer';
 import './styles.scss';
 
 export default function Home() {
     return (
         <div>
-            <Header />
-            <div className='contact'>
-                <Contact />
+            <div className="teste">
+                <Header />
+                <div className='contact'>
+                    <Contact />
+                </div>
+                <About />
+                <ContactIcons />
             </div>
-            <About />
-            <ContactIcons />
+            <div>
+                <Footer />
+            </div>
         </div>
     )
 }
