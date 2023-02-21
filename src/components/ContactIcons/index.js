57500d270e9efa97265448d580bfd93c090445b9
Leandro Furtado <le_furtado@outlook.com.br>
2023-02-21 10:33:40 -0300

    add: criado links de interação nos icones de contato

--- src/components/ContactIcons/index.js
+++ src/components/ContactIcons/index.js
@@ -20,24 +20,28 @@ export default function ContactIcons() {
             titulo: 'E-mail',
             descricao: 'le_furtado@outlook.com.br',
             icone: <FaRegEnvelope size={25} />,
+            link: 'mailto:le_furtado@outlook.com.br'
         },
         {
             id: 3,
             titulo: 'Github',
             descricao: 'lefurtado',
             icone: <FaGithub size={25} />,
+            link: 'https://github.com/lefurtado'
         },
         {
             id: 4,
             titulo: 'WhatsApp',
             descricao: '(11) 97181-5551',
             icone: <FaWhatsapp size={25} />,
+            link: 'https://wa.me/5511971815551'
         },
         {
-            id: 4,
+            id: 5,
             titulo: 'LinkedIn',
-            descricao: 'linkedin.com/in/le-furtado/',
+            descricao: 'linkedin.com/in/le-furtado',
             icone: <FaLinkedinIn size={25} />,
+            link: 'https://www.linkedin.com/in/le-furtado/'
         }
     ]
 
@@ -45,15 +49,17 @@ export default function ContactIcons() {
         <div className='main-container-icon flex'>
 
             {iconComp.map((icon) => (
-                <div key={icon.id} className='icon-container flex'>
-                    <div className="circle flex">
-                        <div className="icon">
-                            {icon.icone}
+                <a key={icon.id} href={icon.link} target='_blank' rel='noreferrer'>
+                    <div className='icon-container flex'>
+                        <div className="circle flex">
+                            <div className="icon">
+                                {icon.icone}
+                            </div>
                         </div>
+                        <h2>{icon.titulo}</h2>
+                        <p>{icon.descricao}</p>
                     </div>
-                    <h2>{icon.titulo}</h2>
-                    <p>{icon.descricao}</p>
-                </div>
+                </a>
             ))}
         </div>
     )
