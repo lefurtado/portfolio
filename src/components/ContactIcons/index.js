9b94177c5f4efa974e2af8d6e4fbd0deba7fb096
Leandro Furtado <le_furtado@outlook.com.br>
2023-03-06 21:40:10 -0300

    mod: retirado icone de smile com meu nome já que agora meu nome aparece principalmente na seção "sobre mim"

--- src/components/ContactIcons/index.js
+++ src/components/ContactIcons/index.js
@@ -3,7 +3,6 @@ import {
     FaWhatsapp,
     FaRegEnvelope,
     FaGithub,
-    FaRegSmileWink,
     FaLinkedinIn
 } from 'react-icons/fa';
 
@@ -11,33 +10,27 @@ export default function ContactIcons() {
     const iconComp = [
         {
             id: 1,
-            titulo: 'Meu nome',
-            descricao: 'Leandro Furtado',
-            icone: <FaRegSmileWink size={25} />,
-        },
-        {
-            id: 2,
             titulo: 'E-mail',
             descricao: 'le_furtado@outlook.com.br',
             icone: <FaRegEnvelope size={25} />,
             link: 'mailto:le_furtado@outlook.com.br'
         },
         {
-            id: 3,
+            id: 2,
             titulo: 'Github',
             descricao: 'lefurtado',
             icone: <FaGithub size={25} />,
             link: 'https://github.com/lefurtado'
         },
         {
-            id: 4,
+            id: 3,
             titulo: 'WhatsApp',
             descricao: '(11) 97181-5551',
             icone: <FaWhatsapp size={25} />,
             link: 'https://wa.me/5511971815551'
         },
         {
-            id: 5,
+            id: 4,
             titulo: 'LinkedIn',
             descricao: 'linkedin.com/in/le-furtado',
             icone: <FaLinkedinIn size={25} />,
