940c794f51db037f660d6199adbe4af39f555dc4
Leandro Furtado <le_furtado@outlook.com.br>
2023-05-22 21:32:14 -0300

    mod: refatorado código de estilo da Navbar

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -1,5 +1,23 @@
-import { NavbarStyled } from './Navbar.styles';
 import { Link } from 'react-scroll';
+import styled from 'styled-components';
+
+const NavbarStyled = styled.ul`
+    display: flex;
+    width: 450px;
+    justify-content: space-between;
+
+    & li a {
+        text-decoration: none;
+        color: ${({theme}) => theme.colors.white};
+        transition: all 100ms;
+        cursor: pointer;
+    }
+
+    & li a:hover {
+        color: ${({theme}) => theme.colors.greenLight};
+        transition: all 100ms;
+    }
+`
 
 export default function Navbar() {
     return (
