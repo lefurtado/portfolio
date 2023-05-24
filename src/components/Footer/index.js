280fe93f8f9253921d3c019d3b16135814b863d5
Leandro Furtado <le_furtado@outlook.com.br>
2023-05-23 21:23:43 -0300

    footer refatorado para styled components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,38 @@
 import './styles.scss';
+import styled from 'styled-components';
+
+const FooterStyles = styled.div`
+    display: flex;
+    height: 40px;
+    background-color: ${({theme}) => theme.colors.gray400};
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 56px;
+
+    & p {
+    font-size: 12px;
+    text-align: center;
+    color: ${({theme}) => theme.colors.white};
+    }
+
+    & span {
+        color: ${({theme}) => theme.colors.greenLight};
+    }
+
+    @media(min-width: 1280px) {
+        height: 60px;
+        margin-top: 120px;
+
+        & p {
+            font-size: 16px;
+        }
+    }
+`
 
 export default function Footer() {
     return (
-        <div className='footer flex'>
+        <FooterStyles>
             <p>Orgulhosamente feito por <span>Leandro Furtado</span></p>
-        </div>
+        </FooterStyles>
     )
 }
