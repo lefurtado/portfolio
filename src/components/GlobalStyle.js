0fac1a9fa736754ca09fb0aebd7761b8336d3da6
Leandro Furtado <le_furtado@outlook.com.br>
2023-05-23 21:39:30 -0300

    corrigido nomenclatura da fonte

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     body {
-        font-family: 'DM Sans', sans-serif;
+        font-family: 'DM sans', sans-serif;
         background-color: ${({theme}) => theme.colors.dark};
     }
 `
