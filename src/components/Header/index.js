6694d6f9c08477ab9f66838c1114df8b6e0af2a5
Leandro Furtado <le_furtado@outlook.com.br>
2023-05-24 21:07:06 -0300

    Header com styled components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,61 @@
 import Hamburger from './Hamburger';
 import Navbar from './Navbar';
-import './styles.scss';
+// import './styles.scss';
+import styled from 'styled-components';
+
+const HeaderStyle = styled.div`
+    display: flex;
+    margin-top: 24px;
+    justify-content: space-between;
+
+    @media(min-width: 1280px) {
+        background-color: ${({theme}) => theme.colors.dark};
+        align-items: center;
+        margin: auto;
+        height: 100px;
+        position: sticky;
+        top: 0;
+        z-index: 6;
+    }
+`
+
+const PortfolioStyle = styled.div`
+    font-family: 'DM Sans', sans-serif;
+    font-weight: 700;
+    font-size: 14px;
+    color: ${({theme}) => theme.colors.white};
+
+    @media(min-width: 1280px) {
+        font-size: 26px;
+    }
+`
+
+const HamburgerMenuStyle = styled.div`
+    @media(min-width: 1280px) {
+        display: none;
+    }
+`
+
+const NavbarMenuStyle = styled.div`
+    display: none;
+
+    @media(min-width: 1280px) {
+        display: block;
+    }
+`
 
 export default function Header() {
     return (
-        <div className='header flex'>
-            <div className='portfolio'>
+        <HeaderStyle>
+            <PortfolioStyle>
                 Portfólio
-            </div>
-            <div className='hamburger_menu'>
+            </PortfolioStyle>
+            <HamburgerMenuStyle>
                 <Hamburger />
-            </div>
-            <div className='navbar_menu'>
+            </HamburgerMenuStyle>
+            <NavbarMenuStyle>
                 <Navbar />
-            </div>
-        </div>
+            </NavbarMenuStyle>
+        </HeaderStyle>
     )
 }
