a350f26aca3f0c5313678c2dd1dda6f2f63dd1ab
Leandro Furtado <le_furtado@outlook.com.br>
2023-05-25 19:43:43 -0300

    mod: modal passado para styled components

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,40 +1,94 @@
-import './styles.scss';
+// import './styles.scss';
 import { AiOutlineCloseSquare } from 'react-icons/ai';
+import styled from 'styled-components';
+
+const ModalContainerStyle = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    width: 1300px;
+    height: 500px;
+    background-color: ${({theme}) => theme.colors.gray400};
+    transform: translate(-50%, -50%);
+`
+
+const ContainerVideoStyle = styled.div`
+    color: ${({theme}) => theme.colors.white};
+    padding: 20px;
+    gap: 1.5rem;
+`
+
+const TitleContainerStyle = styled.div`
+    margin-top: 20px;
+`
+
+const CloseBtnStyle = styled.div`
+    cursor: pointer;
+    position: fixed;
+    right: 2px;
+    top: 2px;
+    color: ${({theme}) => theme.colors.greenLight};
+`
+
+const DescriptionContentStyle = styled.div`
+    margin-top: 10px;
+    color: ${({theme}) => theme.colors.gray};
+    line-height: 1.5rem;
+`
+
+const LinkStyle = styled.div`
+    display: flex;
+    margin-left: 20px;
+    text-align: center;
+    width: 400px;
+    justify-content: space-between;
+
+    & a {
+        color: ${({theme}) => theme.colors.dark};
+        text-decoration: none;
+    }
+`
+
+const BtnStyle = styled.div`
+    background-color: ${({theme}) => theme.colors.greenLight};
+    padding: 10px;
+    width: 150px;
+`
 
 export default function Modal({ isOpen, setModalOpen, titulo, video, descricao, deploy, github }) {
 
     if (isOpen) {
         return (
             <>
-                <div className='modal_container'>
-                    <div className='modal_teste flex'>
-                        <div className="video">
+                <ModalContainerStyle>
+                    <ContainerVideoStyle>
+                        <div>
                             <iframe title='projeto' src={video} width="660" height="403" allow="autoplay"></iframe>
                         </div>
-                        <div className="description_container">
-                            <div className='title_container'>
-                                <div className="project_title">
+                        <div>
+                            <TitleContainerStyle>
+                                <div>
                                     <h2>{titulo}</h2>
                                 </div>
-                                <div className='close_btn'>
+                                <CloseBtnStyle>
                                     <AiOutlineCloseSquare onClick={setModalOpen} size={35} />
-                                </div>
+                                </CloseBtnStyle>
 
-                            </div>
-                            <div className='description_content'>
+                            </TitleContainerStyle>
+                            <DescriptionContentStyle>
                                 {descricao}
-                            </div>
+                            </DescriptionContentStyle>
                         </div>
-                    </div>
-                    <div className='links flex'>
-                        <div className="deploy">
+                    </ContainerVideoStyle>
+                    <LinkStyle>
+                        <BtnStyle>
                             <a href={deploy} target='_blank' rel='noreferrer'>Acessar projeto</a>
-                        </div>
-                        <div className="github">
+                        </BtnStyle>
+                        <BtnStyle>
                             <a href={github} target='_blank' rel='noreferrer'>Acessar repositório</a>
-                        </div>
-                    </div>
-                </div>
+                        </BtnStyle>
+                    </LinkStyle>
+                </ModalContainerStyle>
             </>
         )
     }
