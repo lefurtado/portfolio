3fdbb5b68afe891ea4a629478d7eef407590abd5
Leandro Furtado <le_furtado@outlook.com.br>
2023-05-24 21:36:44 -0300

    fix: modificado ordem dos componentes de estilização por conta de bug na header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -37,8 +37,8 @@ export default function Home() {
 
     return (
         <FontStyle>
+            <Header />
             <PageContainerStyle>
-                <Header />
                 <ContactStyle>
                     <Contact />
                 </ContactStyle>
