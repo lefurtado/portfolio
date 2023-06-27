584c34e4117e99f882a8b0908b70930a5f3f58bd
Leandro Furtado <le_furtado@outlook.com.br>
2023-06-27 15:03:33 -0300

    feat: 'Alteração no estilo global para modificar a barra de rolagem da página'

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -3,7 +3,22 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     body {
-        font-family: 'DM sans', sans-serif;
         background-color: ${({theme}) => theme.colors.dark};
+
+        ::-webkit-scrollbar {
+            width: 10px;
+
+            ::hover {
+                scale: 1.1;
+            }
+        }
+
+        ::-webkit-scrollbar-track {
+            background-color: rgba(29,42,69,1);
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: #BFD7EA;
+        }
     }
 `
